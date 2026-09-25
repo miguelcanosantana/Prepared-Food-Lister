a839889858fa03fef46ca258f83537a8ba3b757d
Miguel Cano <55782155+miguelcanosantana@users.noreply.github.com>
2026-09-26 00:48:10 +0200

    Base of modal.

--- src/app/pages/ingredients/ingredients.page.ts
+++ src/app/pages/ingredients/ingredients.page.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
-import { IonContent, IonHeader, IonTitle, IonFab, IonFabButton, IonIcon, IonInput, IonToolbar, IonButtons, IonButton, IonModal, IonItem } from '@ionic/angular';
+import { IonContent, IonHeader, IonTitle, IonFab, IonFabButton, IonIcon, IonInput, IonToolbar, IonButtons, IonButton, IonModal, IonItem, IonRow, IonCol, IonGrid } from '@ionic/angular';
 import { addIcons } from 'ionicons';
 import { add } from 'ionicons/icons';
+import { Ingredient } from '../../models/ingredient';
 
 @Component({
   selector: 'app-ingredients',
   templateUrl: './ingredients.page.html',
-  styleUrls: ['./ingredients.page.scss'],
-  imports: [IonItem, IonModal, IonTitle, IonButton, IonButtons, IonToolbar, IonInput, IonContent, IonHeader, IonFab, IonFabButton, IonIcon],
+  imports: [IonItem, IonModal, IonTitle, IonButton, IonButtons, IonToolbar, IonInput, IonContent, IonHeader, IonFab, IonFabButton, IonIcon, IonRow, IonCol, IonGrid]
 })
 export class IngredientsPage {
-  testButtonClicked = false;
 
-    constructor() {
-      addIcons({ add });
-    }
+  newIngredient?: Ingredient
+
+  constructor() {
+    addIcons({ add });
+  }
 }
