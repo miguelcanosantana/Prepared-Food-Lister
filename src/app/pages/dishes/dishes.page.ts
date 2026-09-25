d3140de17a71ff6536a84c6e5563a9a9a9bedfda
Miguel Cano <55782155+miguelcanosantana@users.noreply.github.com>
2026-09-26 00:27:24 +0200

    Fixed routing. Initial modal.

--- src/app/pages/dishes/dishes.page.ts
+++ src/app/pages/dishes/dishes.page.ts
@@ -1,21 +1,15 @@
 import { Component, inject } from '@angular/core';
-import { RefresherCustomEvent, IonHeader, IonToolbar, IonTitle, IonContent, IonRefresher, IonRefresherContent, IonList } from '@ionic/angular';
+import { IonHeader, IonContent, IonList, IonButton } from '@ionic/angular';
 
 import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-dishes',
   templateUrl: 'dishes.page.html',
-  styleUrls: ['dishes.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonRefresher, IonRefresherContent, IonList],
+  imports: [IonButton, IonHeader, IonContent, IonList],
 })
 export class DishesPage {
   
   private data = inject(DataService);
 
-  refresh(ev: any) {
-    setTimeout(() => {
-      (ev as RefresherCustomEvent).detail.complete();
-    }, 3000);
-  }
 }
--- src/app/pages/ingredients/ingredients.page.ts
+++ src/app/pages/ingredients/ingredients.page.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular';
+import { IonContent, IonHeader, IonTitle, IonFab, IonFabButton, IonIcon, IonInput, IonToolbar, IonButtons, IonButton, IonModal, IonItem } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons';
 
 @Component({
   selector: 'app-ingredients',
   templateUrl: './ingredients.page.html',
   styleUrls: ['./ingredients.page.scss'],
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar],
+  imports: [IonItem, IonModal, IonTitle, IonButton, IonButtons, IonToolbar, IonInput, IonContent, IonHeader, IonFab, IonFabButton, IonIcon],
 })
-export class IngredientsPage {}
+export class IngredientsPage {
+  testButtonClicked = false;
+
+    constructor() {
+      addIcons({ add });
+    }
+}
