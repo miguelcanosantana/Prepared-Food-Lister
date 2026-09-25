7283a2d9c4ae0c5a49aaefe7d741fef18a57c4d2
Miguel Cano <55782155+miguelcanosantana@users.noreply.github.com>
2026-09-25 18:55:40 +0200

    Created model classes.

---
+++ src/app/models/dish.ts
@@ -0,0 +1,11 @@
+import { Ingredient } from './ingredient';
+
+export class Dish {
+  constructor(
+    public guid: string,
+    public name: string,
+    public ingredients: Ingredient[],
+    public description?: string,
+    public servings?: number,
+  ) {}
+}
---
+++ src/app/models/ingredient.ts
@@ -0,0 +1,9 @@
+export class Ingredient {
+  constructor(
+    public guid: string,
+    public name: string,
+    public quantity?: number,
+    public unit?: string,
+    public kcal?: number
+  ) {}
+}
