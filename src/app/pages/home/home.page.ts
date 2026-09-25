4fa89451f6477bc1c083447dab1403e16bee02d4
Miguel Cano <55782155+miguelcanosantana@users.noreply.github.com>
2026-09-25 17:00:42 +0200

    Deleted unnecesary classes.

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,18 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { RefresherCustomEvent, IonHeader, IonToolbar, IonTitle, IonContent, IonRefresher, IonRefresherContent, IonList } from '@ionic/angular';
-import { MessageComponent } from '../message/message.component';
 
-import { DataService } from '../services/data.service';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonRefresher, IonRefresherContent, IonList, MessageComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonRefresher, IonRefresherContent, IonList],
 })
 export class HomePage {
+  
   private data = inject(DataService);
-  protected readonly messages = this.data.getMessages();
 
   refresh(ev: any) {
     setTimeout(() => {
